6bb1de830eea4d56970ca2e20546766e72d115bb
winzhy <42858366+winzhy@users.noreply.github.com>
2018-08-31 17:18:59 +0800

    Add files via upload

---
+++ walker.chat.cmd
@@ -0,0 +1,19 @@
+@echo off
+color 02
+echo Please do not use IE browser!!!
+echo.
+echo. 
+set /p NO=Please input your number¡ª¡ª#
+cls
+echo      Welcome......
+echo   walker#%NO%
+ping 127.0.0.1 -n 5 >nul
+cls
+echo wait......
+start www.hack.chat/?walker#%NO%
+ping 127.0.0.1 -n 5 >nul
+cls
+echo finish!!!
+pause >nul
+exit 
+ 
